fab42340b5393114493f0c7f9c62a27eb5f5c7b3
Jacob Lee <jelee231@gmail.com>
2024-10-29 14:43:51 -0400

    added match time and battery voltage driver station

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import java.io.File;
@@ -67,6 +70,8 @@ public class Robot extends TimedRobot
 		// and running subsystem periodic() methods.  This must be called from the robot's periodic
 		// block in order for anything in the Command-based framework to work.
 		CommandScheduler.getInstance().run();
+		SmartDashboard.putNumber("Match Time", DriverStation.getMatchTime());
+		SmartDashboard.putNumber("Battery Voltage", RobotController.getBatteryVoltage());
 	}
 
 	/**
