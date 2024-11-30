6d8d0e110781a55f6067bd6a77438b9e3c29c911
Jacob Lee <jelee231@gmail.com>
2024-11-29 23:53:45 -0500

    removed unnecessary line

--- src/main/java/org/cometrobotics/frc2024/robot/Robot.java
+++ src/main/java/org/cometrobotics/frc2024/robot/Robot.java
@@ -74,8 +74,7 @@ public class Robot extends LoggedRobot
 		this.recordMetadata();
 		this.configureDataReceivers();
 		this.configureCommandLogging();
-		// Logger.disableDeterministicTimestamps() // See "Deterministic Timestamps" in the "Understanding Data Flow" page
-		Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+		Logger.start();
 	}
 
 	private void recordMetadata() {
